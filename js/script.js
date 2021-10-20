2d9ce161f3399b12a45e1a4f2542df655eafb597
Logan W <loganlwright@gmail.com>
2021-10-20 18:49:23 -0400

    Ability to change grid size

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 const gridContainer = document.querySelector(".grid-container");
+const input = document.querySelector(".input");
 
-function makeRows(rows, cols) {
+const makeRows = (rows, cols) => {
   gridContainer.style.setProperty("--grid-rows", rows);
   gridContainer.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
     gridContainer.appendChild(cell).className = "grid-item";
   }
-}
-makeRows(16, 16);
+};
+makeRows(4, 4);
 
-const gridItems = document.querySelectorAll(".grid-item");
+const getGridItems = () => {
+  const gridItems = document.querySelectorAll(".grid-item");
+  return gridItems;
+};
 
-gridItems.forEach((item) => {
+input.addEventListener("change", function (e) {
+  let gridSize = e.target.value;
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  makeRows(gridSize, gridSize);
+  getGridItems().forEach((item) => {
+    item.addEventListener("mouseenter", function (e) {
+      e.target.style.backgroundColor = "black";
+    });
+  });
+});
+
+getGridItems().forEach((item) => {
   item.addEventListener("mouseenter", function (e) {
     e.target.style.backgroundColor = "black";
   });
