e2e6d78e0085c911d106795b988bfebf8f3403d4
Logan W <loganlwright@gmail.com>
2021-10-24 22:19:38 -0400

    ui changes

--- js/script.js
+++ js/script.js
@@ -1,44 +1,78 @@
-const gridContainer = document.querySelector(".gridContainer");
+const DEFAULT_COLOR = "#333333";
+const DEFAULT_MODE = "Color";
+const DEFAULT_SIZE = 32;
 
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
+const gridContainer = document.querySelector(".gridContainer");
 const sliderValue = document.querySelector("#sliderValue");
-const colorPicker = document.querySelector(".colorPicker");
+const colorPicker = document.querySelector("#colorPickerButton");
+const rainbowButton = document.querySelector("#rainbowButton");
+const colorButton = document.querySelector("#colorButton");
+const eraserButton = document.querySelector("#eraserButton");
+const clearButton = document.querySelector("#clearButton");
 const slider = document.querySelector("#slider");
 
-const addMouseEnterListeners = () => {
-  getGridItems().forEach((item) => {
-    item.addEventListener("mouseenter", function (e) {
-      e.target.style.backgroundColor = colorPicker.value;
-    });
-  });
+const setCurrentColor = (newColor) => {
+  currentColor = newColor;
 };
+
+const setCurrentSize = (newSize) => {
+  currentSize = newSize;
+};
+
+const setCurrentMode = (newMode) => {
+  currentMode = newMode;
+};
+
+colorPicker.onchange = (e) => setCurrentColor(e.target.value);
+colorButton.onclick = () => setCurrentMode("Color");
+rainbowButton.onclick = () => setCurrentMode("Rainbow");
+eraserButton.onclick = () => setCurrentMode("Eraser");
+clearButton.onclick = () => reloadGrid();
 
 const makeRows = (rows, cols) => {
   gridContainer.style.setProperty("--grid-rows", rows);
   gridContainer.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
+    cell.addEventListener("mouseenter", colorChange);
     gridContainer.appendChild(cell).className = "grid-item";
   }
 };
 
-const getGridItems = () => {
-  const gridItems = document.querySelectorAll(".grid-item");
-  return gridItems;
-};
-
-// need to change this so it only updates afte the slider is done
-slider.onchange = function () {
-  let gridSize = this.value;
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.firstChild);
+const colorChange = (e) => {
+  if (currentMode === "Rainbow") {
+    let valueOne = Math.floor(Math.random * 256);
+    let valueTwo = Math.floor(Math.random * 256);
+    let valueThree = Math.floor(Math.random * 256);
+    e.target.backgroundColor = `rgb(${valueOne},${valueTwo},${valueThree})`;
+  } else if (currentMode === "Color") {
+    e.target.style.backgroundColor = currentColor;
   }
-  makeRows(gridSize, gridSize);
-  addMouseEnterListeners();
-};
-slider.oninput = function () {
-  let gridSize = this.value;
-  sliderValue.textContent = `${gridSize}x${gridSize}`;
 };
 
-makeRows(16, 16);
-addMouseEnterListeners();
+const activateButton = (newMode) => {
+  if (currentMode === "Rainbow") {
+    rainbowButton.classList.remove("btn-down");
+  } else if (currentMode === "Color") {
+    colorButton.classList.remove("btn-down");
+  } else if (currentMode === "Eraser") {
+    eraserButton.classList.remove("btn-down");
+  }
+
+  if (newMode === "Rainbow") {
+    rainbowButton.classList.add("btn-down");
+  } else if (newMode === "Color") {
+    colorButton.classList.add("btn-down");
+  } else if (newMode === "Eraser") {
+    eraserButton.classList.add("btn-down");
+  }
+};
+
+window.onload = () => {
+  makeRows(DEFAULT_SIZE, DEFAULT_SIZE);
+  activateButton(DEFAULT_MODE);
+};
